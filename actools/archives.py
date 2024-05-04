65265120ce3e0a9a6366b68941859056474fde44
Théo MEEDENDORP <tmeedend@gmail.com>
2024-05-04 12:57:36 +0200

    update

--- actools/archives.py
+++ actools/archives.py
@@ -161,6 +161,375 @@ def transformToValidMod(params, archiveToProcess):
     else:
         print("No mod found")
 
+
+def fixTrackTags(modDir, mo2categories):
+    tagsToRemove = ['1966', '1988', '1994', '2001', '2021', 'acc', 'bollene','carlo','canada','carrera.4','chicago','chiquifreaky',"chq",'dragon','euroracers','exeqt0r','imola',
+    'interlagos','fat-alfie','germany','gpl','hockenheim', 'lemax',
+    'mod','monte','monaco','moscow','pau', 'permanent racing circuit', 'quebec','rainmaker', 'remake','rmi','san francisco', "saudi arabia",
+    'superstage', 'shibuya', 'trail', 'tyrone','vesubie', "yeda", "yokohama"]
+    if mo2categories == None:
+        mo2categories = []
+    layouts = []
+    layoutsOk = []
+    layouts.append(os.path.join(modDir, "ui", "ui_track.json"))
+
+    for file in os.listdir(os.path.join(modDir, "ui")):
+        layouts.append(os.path.join(modDir, "ui", file, "ui_track.json"))
+
+    for layoutJsonFile in layouts:
+        if os.path.isfile(layoutJsonFile):
+            try:
+                jsonData = common.parseJson(layoutJsonFile)
+            except Exception as e: 
+                print(e)
+                print ("Cannot load ui_track.json to fix track for json file " + layoutJsonFile)
+                return
+            layoutTags = jsonData['tags'] 
+            modUpdatedTagsSet = set()
+            for tag in layoutTags:
+                tag = tag.lower()
+                if not tag in tagsToRemove:
+                    if tag == 'freeroam':
+                        modUpdatedTagsSet.add('#freeroam')
+                    elif tag == '#circuit':
+                        modUpdatedTagsSet.add('circuit')
+                    elif tag == 'country':
+                        modUpdatedTagsSet.add('country-side')
+                    elif tag == '24h' or tag == 'sebring':
+                        modUpdatedTagsSet.add('endurance')
+                    elif tag == 'grand prix':
+                        modUpdatedTagsSet.add('gp')
+                    elif tag == 'city':
+                        modUpdatedTagsSet.add('street')
+                    elif tag == 'dockland':
+                        modUpdatedTagsSet.add('docks')
+                    elif tag == 'formula 1':
+                        modUpdatedTagsSet.add('f1')
+                    elif tag == 'historic gp':
+                        modUpdatedTagsSet.add('historic')
+                        modUpdatedTagsSet.add('gp')
+                    elif tag == 'p2p':
+                        modUpdatedTagsSet.add('a2b')
+                    elif tag == 'drift':
+                        modUpdatedTagsSet.add('#drift')
+                    elif tag == 'hillclimb' or tag == 'hill climb':
+                        modUpdatedTagsSet.add('hillclimb')
+                        modUpdatedTagsSet.add('a2b')
+                        modUpdatedTagsSet.add('#rally')
+                    elif tag == 'rally' or tag == 'rallye' or tag == 'wrc' or tag == 'stage' or tag == 'speciale':
+                        modUpdatedTagsSet.add('#rally')
+                    elif tag == 'dirt3' or tag == 'fm7' or tag == 'grid' or tag == 'grid2' or tag == 'grid autosport' or tag == 'forza motorsport':
+                        modUpdatedTagsSet.add('fictional')
+                    else:
+                        modUpdatedTagsSet.add(tag)
+            modName = jsonData['name']
+            
+            for cat in mo2categories:
+                if cat == 'Track - Rally':
+                    modUpdatedTagsSet.add('#rally')
+                elif cat == 'Track - Freeroam':
+                    modUpdatedTagsSet.add('#freeroam')
+                elif cat == 'Circuit - Endurance':
+                    modUpdatedTagsSet.add('endurance')
+                elif cat == 'Circuit - Vintage':
+                    modUpdatedTagsSet.add('vintage')
+                elif cat == 'Circuit - Drift':
+                    modUpdatedTagsSet.add('#drift')
+
+            modUpdatedTags= []
+            # copy set in array
+            for tag in modUpdatedTagsSet:
+                modUpdatedTags.append(tag)
+
+            jsonData.update({'tags':modUpdatedTags})
+            try:
+                with open(layoutJsonFile, "w") as jsonFile:
+                    json.dump(jsonData, jsonFile, indent=2)
+            except Exception as e: 
+                print(e)
+                print ("Cannot save new tags to fix track for mod " + modName)
+
+def fixCarTags(modDir, mo2categories):
+    metadataFilePath =  os.path.join(modDir, "ui", "ui_car.json")
+    try:
+        jsonData = common.parseJson(metadataFilePath)
+    except Exception as e: 
+        print(e)
+        print ("Cannot load ui_car.json to fix car for mod " + modDir)
+        return
+    modTags = jsonData['tags']
+    if mo2categories == None:
+        mo2categories = []
+    modName = jsonData['name']
+    modNameLower = modName.lower()
+    
+    modClass = jsonData['class'].lower()
+    modBrand =  jsonData['brand']
+
+    ###### fix brand ######
+    if "bayro" in modNameLower:
+        modBrand = "BMW"
+    elif "auriel" in modNameLower:
+        modBrand = "Audi"
+    elif "darche" in modNameLower:
+        modBrand = "Porsche"
+    elif "minardi" in modNameLower:
+        modBrand = "Minardi"
+    elif "bar "in modNameLower:
+        modBrand = "Bar"
+
+    modUpdatedTagsSet = set()
+    # guess tags from name
+    if modNameLower.__contains__('police'):
+        modUpdatedTagsSet.add('police')
+    if modNameLower.__contains__('taxi'):
+        modUpdatedTagsSet.add('taxi')
+    if modNameLower.__contains__('traffic'):
+        modUpdatedTagsSet.add('#traffic')
+    if modNameLower.__contains__('truck'):
+        modUpdatedTagsSet.add('#truck')
+
+    # modYear = jsonData['year']
+
+    ###### fix class ######
+    if modClass != 'race' and modClass != 'street':
+        if modClass == 'racing' or modClass == 'grand prix' or modClass == 'dpi' or modClass == 'group 6 gen 1' or modClass == 'group 5 gen 3' or modClass == 'group 5' or modClass == 'cup' or modClass == 'gt' or modClass == 'ac legends' or modClass == 'f2':
+            modClass = 'race'
+        elif modClass == 'drift' or  modClass == 'street drift' :
+            modUpdatedTagsSet.add('#drift')
+        elif modClass == 'rally' or modClass == 'hillclimb' or modClass == 'pikespeak' or modClass == 'kit-car':
+            modUpdatedTagsSet.add('#rally')
+            modClass = 'race'
+        elif modClass == 'stock' or modClass == 'tuning' or modClass == 'ev' or modClass == 'shutoko' or modClass == 'touge' or modClass == 'street/trackday' or modClass == 'tuned' or modClass == 'wangan':
+            modClass = 'street'
+        elif modClass == 'rally gr4':
+            modUpdatedTagsSet.add('#rally')
+            modUpdatedTagsSet.add('group 4')
+            modClass = 'race'
+        elif modClass == 'tcr':
+            modUpdatedTagsSet.add('#tcr')
+            modClass = 'race'
+        elif modClass == 'wtcr':
+            modUpdatedTagsSet.add('#wtcr')
+            modClass = 'race'
+        elif modClass == 'gt500':
+            modUpdatedTagsSet.add('#gt500')
+            modClass = 'race'
+        elif modClass == 'dtm':
+            modUpdatedTagsSet.add('#dtm')
+            modClass = 'race'
+        elif modClass == 'group 5':
+            modClass = 'race'
+        elif modClass == 'formula 3':
+            modUpdatedTagsSet.add('#formula 3')
+            modClass = 'race'
+        elif modClass == 'prototype' or modClass == 'group c':
+            modUpdatedTagsSet.add('prototype')
+            modClass = 'race'
+        elif modClass == 'traffic':
+            modUpdatedTagsSet.add('#traffic')
+            modClass = 'street'
+        elif modClass == 'historic' or modClass == 'vintage':
+            modUpdatedTagsSet.add('vintage')
+            modClass = 'race'
+        elif modClass == 'utility':
+            modUpdatedTagsSet.add('#utility')
+            modClass = 'street'
+        else:
+            print ("Warning, car " + modName + " has invalid class " + modClass)
+    
+
+    ###### fix tags ######
+    tagsToRemove = ['1923','1.4 inline-4 8v', '5-speed', '300zx','718', '911', '993', '1973', '1990s', '#a1','#a5', 'a1', 'ap1', 'a2','a3dr', 'a5','alpine','amg',
+     'audi','assettocorsamods', 'bmw', 'california','camaro', 'car','carrera','ceky perfomance','ceky performance','c-one','cayman', 'civic','dp', 'ddm','evo','e36','ek9','euro pack', 'evo4',
+      'f5', 
+    'fd2', 'france', 'fk 11','fiat',
+    'germany','gts-r','gt+2.0', 'gue', 'great britain','gapplebees','gt3 rs','gtr','guerilla mods','honda','italy','ita','japan', 'koldo83', 'lambo','lms', 'ltk','legend', 'legends',
+    'm4', 'm3', 'm power', 'mr', 'mrs', 'ma70', 'mod', 'mugen', 'n/a', 'nissan', 'nsx',
+     'p.verdes','pm3dm','peugeot', 'public release', 'physics v3', 'porsche','prelude','r1',
+    'rb25', 'r31', 'r32', 'r8', 'race', 'racing software', 'rollovers', 'romania','#rss', 'r1', 'rs', 'rsr',
+    'rss', 's1', 's3.0','s15','scorpion', 'sin', 'sl63', 'stock', 'street',  'supra', 'silvia', 'singer', 'shutoko','s2000','skyline','toyota','tm-modding', 'track', 'ttrs', 'type r', 'usa', 'vantage',
+    'x-bow', 'wip', 'zzw30']
+    for tag in modTags:
+        tag = tag.lower()
+        if not tag in tagsToRemove:
+            if tag == 'rally' or tag == 'rallye' or tag == '#rallye' or tag == 'hillclimb' or tag == 'wrc' or tag == '#rally wrc':
+                modUpdatedTagsSet.add('#rally')
+            elif tag == 'lmp1':
+                modUpdatedTagsSet.add('#lmp1')
+                modUpdatedTagsSet.add('prototype')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'lmp2':
+                modUpdatedTagsSet.add('#lmp2')
+                modUpdatedTagsSet.add('prototype')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'lmp900':
+                modUpdatedTagsSet.add('#lmp900')
+                modUpdatedTagsSet.add('prototype')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'lmgt':
+                modUpdatedTagsSet.add('#lmgt')
+                modUpdatedTagsSet.add('gt1')
+                modUpdatedTagsSet.add('gt')
+                modUpdatedTagsSet.add('vintage')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'lmgtp':
+                modUpdatedTagsSet.add('#lmgtp')
+                modUpdatedTagsSet.add('prototype')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'f1' or tag.startswith("formula 1") or tag.startswith("#formula 1"):
+                modUpdatedTagsSet.add("#formula 1")
+                modUpdatedTagsSet.add('openwheeler')
+                modUpdatedTagsSet.add('singleseater')
+            elif tag == 'f2' or tag.startswith("formula 2") or tag.startswith("#formula 2"):
+                modUpdatedTagsSet.add("#formula 2")
+                modUpdatedTagsSet.add('openwheeler')
+                modUpdatedTagsSet.add('singleseater')
+            elif tag == 'f3' or tag.startswith("formula 3") or tag.startswith("#formula 3"):
+                modUpdatedTagsSet.add("#formula 3")
+                modUpdatedTagsSet.add('openwheeler')
+                modUpdatedTagsSet.add('singleseater')
+            elif tag == 'formula ford':
+                modUpdatedTagsSet.add('#formula ford')
+            elif tag == 'btcc':
+                modUpdatedTagsSet.add('#btcc')
+            elif tag == 'dpi' or tag == '#dpi':
+                modUpdatedTagsSet.add('dpi')
+            elif tag == 'drm':
+                modUpdatedTagsSet.add('#drm')
+            elif tag == 'grand prix':
+                modUpdatedTagsSet.add('gp')
+            elif tag == 'lemans' or tag == 'le mans':
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'gra':
+                modUpdatedTagsSet.add('#rally')
+                modUpdatedTagsSet.add('group 3')
+            elif tag == 'dtm':
+                modUpdatedTagsSet.add('#dtm')
+            elif tag == '#gte'or tag == 'gte' :
+                modUpdatedTagsSet.add('#lm-gte')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'gt2' or tag == '#gt2' :
+                modUpdatedTagsSet.add('#gt2')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'gt4':
+                modUpdatedTagsSet.add('#gt4')
+            elif tag == 'gt3':
+                modUpdatedTagsSet.add('#gt3')
+            elif tag == 'n-gt':
+                modUpdatedTagsSet.add('#n-gt')
+            elif tag == 'tcr':
+                modUpdatedTagsSet.add('#tcr')
+            elif tag == '#sgt500' or tag == 'jgtc' or tag == 'gt500':
+                modUpdatedTagsSet.add('#gt500')
+            elif tag == 'can-am' or tag == 'can am' or tag == '#can-am' or tag == '#can am':
+                modUpdatedTagsSet.add('#can-am')
+            elif tag == 'traffic':
+                modUpdatedTagsSet.add('#traffic')
+            elif tag == 'sport' or tag == 'roadsport'or tag == 'sportscar'or tag == 'sportcar':
+                modUpdatedTagsSet.add('#sportscars')
+            elif tag == 'open wheeler':
+                modUpdatedTagsSet.add('openwheeler')
+            elif tag == 'single seater':
+                modUpdatedTagsSet.add('singleseater')
+            elif tag == 'historic':
+                modUpdatedTagsSet.add('vintage')
+            elif tag == 'hothatch' or tag == 'hot hatchback':
+                modUpdatedTagsSet.add('hatchback')
+            elif tag == 'supercar' or tag == '#supercar':
+                modUpdatedTagsSet.add('#supercars')
+            elif tag == 'proto c' or tag == '#proto c' or tag == 'prototype c' or tag == 'group c' or tag == '#group c' or tag == '#prototypes grc'  or tag == '#group-c/3.5':
+                modUpdatedTagsSet.add('#prototypes group c')
+                modUpdatedTagsSet.add('prototype')
+                modUpdatedTagsSet.add('endurance')
+            elif tag == 'wangan' or tag == 'jdm':
+                modUpdatedTagsSet.add('#jdm')
+            else:
+                modUpdatedTagsSet.add(tag)
+        #else:
+        #    print("ignoring tag " + tag)
+
+
+    for cat in mo2categories:
+        if cat == 'Car - BTCC':
+            modUpdatedTagsSet.add('#btcc')
+        if cat == 'Car - Rally':
+            modUpdatedTagsSet.add('#rally')
+        elif cat == 'Car - Cup and Misc':
+            modUpdatedTagsSet.add('cup')
+        elif cat == 'Car - GT500':
+            modUpdatedTagsSet.add('#gt500')
+        elif cat == 'Car - GT1':
+            modUpdatedTagsSet.add('gt1')
+        elif cat == 'Car - LMGT':
+            modUpdatedTagsSet.add('#lmgt')
+            modUpdatedTagsSet.add('gt1')
+            modUpdatedTagsSet.add('gt')
+            modUpdatedTagsSet.add('vintage')
+            modUpdatedTagsSet.add('endurance')
+        elif cat == 'Car - LMGTP':
+            modUpdatedTagsSet.add('#lmgtp')
+            modUpdatedTagsSet.add('prototype')
+            modUpdatedTagsSet.add('endurance')
+        elif cat == 'Car - LM-GTE':
+            modUpdatedTagsSet.add('#lm-gte')
+            modUpdatedTagsSet.add('endurance')
+        elif cat == 'Car - N-GT':
+            modUpdatedTagsSet.add('#n-gt')
+        elif cat == 'Car - GT2':
+            modUpdatedTagsSet.add('#gt2')
+        elif cat == 'Car - GT3':
+            modUpdatedTagsSet.add('#gt3')
+        elif cat == 'Car - GT4':
+            modUpdatedTagsSet.add('#gt4')
+        elif cat == 'Car - Proto - LMP1':
+            modUpdatedTagsSet.add('#lmp1')
+            modUpdatedTagsSet.add('prototype')
+            modUpdatedTagsSet.add('endurance')
+        elif cat == 'Car - Proto - LMH':
+            modUpdatedTagsSet.add('#lmh')
+            modUpdatedTagsSet.add('prototype')
+            modUpdatedTagsSet.add('endurance')
+        elif cat == 'Car - Proto - LMP2':
+            modUpdatedTagsSet.add('#lmp2')
+            modUpdatedTagsSet.add('prototype')
+            modUpdatedTagsSet.add('endurance')
+        elif cat == 'Car - Road - Drift':
+            modUpdatedTagsSet.add('#drift')
+        elif cat == 'Car - Formula 1':
+            modUpdatedTagsSet.add("#formula 1")
+            modUpdatedTagsSet.add('openwheeler')
+            modUpdatedTagsSet.add('singleseater')
+        elif cat == 'Car - Indicar':
+            modUpdatedTagsSet.add('openwheeler')
+            modUpdatedTagsSet.add('singleseater')
+            modUpdatedTagsSet.add('#indicar')
+        elif cat == 'Car - Nascar':
+            modUpdatedTagsSet.add('#nascar')
+        elif cat == 'Car - Traffic':
+            modUpdatedTagsSet.add('#traffic')
+        elif cat == 'Car - Road - JDM Culture':
+            modUpdatedTagsSet.add('#jdm')
+        elif cat == 'Car - Proto Group C':
+            modUpdatedTagsSet.add('#prototypes group c')
+            modUpdatedTagsSet.add('prototype')
+            modUpdatedTagsSet.add('endurance')
+    
+    modUpdatedTags= []
+    # copy set in array
+    for tag in modUpdatedTagsSet:
+        modUpdatedTags.append(tag)
+
+    jsonData.update({'tags':modUpdatedTags})
+    jsonData.update({'class':modClass})
+    jsonData.update({'brand':modBrand})
+    try:
+        with open(metadataFilePath, "w") as jsonFile:
+            json.dump(jsonData, jsonFile, indent=2)
+    except Exception as e: 
+        print(e)
+        print ("Cannot save new tags to fix car for mod " + modName)
+
 def recursiveMoveModsToValidModDir(workdir, newModDir, currentDirName):
     # first we check if the first dir level is a mod
     if isTrack(workdir):
--- mo2-overwrite-to-mod.py
+++ mo2-overwrite-to-mod.py
@@ -1,38 +1,102 @@
 import os
-import ntpath
+import shutil 
+from actools import archives
+from actools import common
 
 from actools import params
 
-
-def processMod(paramsToUse, modTool, modsToProcess):
-	if modsToProcess != None and modsToProcess.strip() != "":
-		if modsToProcess == "#all":
-			modTool.packAllMods(paramsToUse, paramsToUse.acpath)
+def recursive_copy(src, dst):
+	if os.path.isdir(src):
+		if not os.path.exists(dst):
+			shutil.copytree(src, dst)
 		else:
-			if modsToProcess.startswith("tags:"):
-				tags = modsToProcess.split("tags:")[1].split(',')
-				mods = modTool.findModsWithTag(paramsToUse.acpath, tags)
-			else:
-				mods = modsToProcess.split(",")
-			for mod in mods:
-				if not modTool.isKunosMod(mod) or not paramsToUse.skipKunosMods:
-					modTool.packMod(mod.strip(), paramsToUse, paramsToUse.acpath)
-	
+			for item in os.listdir(src):
+				s = os.path.join(src, item)
+				d = os.path.join(dst, item)
+				recursive_copy(s,d)
+	elif os.path.isfile(src):
+		if not os.path.exists(dst):
+			shutil.copy2(src, dst)
+		else:
+			print("ignoring copy of exsiting file" + src)
+
+mo2CategoriesConfig = {}
+def initmo2CategoriesConfig(categoriesDat):
+	fileCat = open(categoriesDat, 'r')
+	lines = fileCat.readlines()
+	for line in lines:
+		lineArray = line.strip().split('|')
+		mo2CategoriesConfig[lineArray[0]] = lineArray[1]
+
+
+def readMO2Category(metaIniFile):
+	cats = []
+	if os.path.isfile(metaIniFile):
+		meta = common.readIniFile(metaIniFile)
+		if meta.has_section('General'):
+			if meta.has_option('General', 'category'):
+				categoriesIntStr = meta.get('General', 'category')
+				categoriesIntStr = categoriesIntStr.replace('"', '')
+				for catInt in categoriesIntStr.split(","):
+					catInt = catInt.replace(',', '')
+					if catInt != None and catInt in mo2CategoriesConfig:
+						cats.append(mo2CategoriesConfig[catInt])
+	if len(cats) == 0:
+		print("cannot find MO2 category for meta file " + metaIniFile)
+	return cats
 
 def main():
 	paramsToUse = params.Params(os.path.dirname(os.path.realpath(__file__)))
 	paramsToUse.checkEnv()
 	overwriteDir = os.path.join(paramsToUse.acmodspath, "../overwrite")
-	# read all tracks in mods
+	
+	initmo2CategoriesConfig(os.path.join(paramsToUse.acmodspath, "../categories.dat"))
+	# read all tracks and cars in mods
 	tracks = {}
+	cars = {}
+	mo2Category = {}
 	mods = os.listdir(paramsToUse.acmodspath)
+	index = 0
 	for mod in mods:
 		tracksDir = os.path.join(paramsToUse.acmodspath, mod, "content", "tracks")
+		carsDir = os.path.join(paramsToUse.acmodspath, mod, "content", "cars")
 		if os.path.isdir(tracksDir):
 			for trackname in os.listdir(tracksDir):
-				tracks[trackname] = os.path.join(tracksDir, trackname)
-	print("printing found tracks")
-	for key,value in tracks.items():
-		print(key, ':', value)
+				trackDir = os.path.join(tracksDir, trackname)
+				if not archives.isTrack(trackDir):
+					#print("Warning, ignoring track dir " + trackDir + " because it's not a track")
+					pass
+				elif "Track skin - " in mod or "Track extension - " in mod:
+					#print("Warning, ignoring track dir " + trackDir + " because it has skin or extension in the mod name"
+					pass
+				else:
+					if trackname in tracks:
+						print("Error processing " + trackDir + ", track " + trackname + " already found here: " + tracks[trackname])
+						tracks.pop(trackname)
+					else:
+						tracks[trackname] = trackDir
+						archives.fixTrackTags(trackDir, readMO2Category(os.path.join(paramsToUse.acmodspath, mod, "meta.ini")))
+		if os.path.isdir(carsDir):
+			for carname in os.listdir(carsDir):
+				carDir = os.path.join(carsDir, carname)
+				if archives.isCar(carDir):
+					if carname in cars:
+						print("Error processing " + carDir + ", car " + carname + " already found here:" + cars[carname])
+						cars.pop(carname)
+					else:
+						cars[carname] = carDir
+						archives.fixCarTags(carDir, readMO2Category(os.path.join(paramsToUse.acmodspath, mod, "meta.ini")))
+						index += 1
+
+	tracksOverwriteDir = os.path.join(overwriteDir, "content", "tracks")
+	carsOverwriteDir = os.path.join(overwriteDir, "content", "cars")
+	for trackname in os.listdir(tracksOverwriteDir):
+		if trackname in tracks:
+			recursive_copy(os.path.join(tracksOverwriteDir, trackname), tracks[trackname])
+		shutil.rmtree(os.path.join(tracksOverwriteDir, trackname))
+	for carname in os.listdir(carsOverwriteDir):
+		if carname in cars:
+			recursive_copy(os.path.join(carsOverwriteDir, carname), cars[carname])
+		shutil.rmtree(os.path.join(carsOverwriteDir, carname))
 
 main()
